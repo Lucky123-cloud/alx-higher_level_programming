9b41876d21cb8978062a67139489c89479c88b04
Lucky-cloud 123 <luckybaraka21@gmail.com>
2025-09-17 13:38:00 +0300

    added a afew things

--- fastAPI/main.py
+++ fastAPI/main.py
@@ -1,15 +1,59 @@
 from fastapi import FastAPI #this is importing fast API
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI() #this is creating the instance of FastAPI
 
-@app.get('/') #decoration to define an API
-def index() :
+@app.get('/blog') #decoration to define an API - Called path other languages call it routes
+def index(limit=10, published: bool=True, sort: Optional[str] = None): #path operation function
+    #only get 10 published blogs
+    if published:
+        return {
+        'data': { f'{limit} published blog lists from the DB' }
+    }
+    else:
+        return { 'data': f'{limit} blogs from the db'}
+
+    
+
+
+@app.get('/blog/unpublished')
+def unpublished():
     return {
-        'data': {'name': 'Lucky Baraka'}
+        'data': 'Unpublished Data'
     }
 
-@app.get('/about')
+@app.get('/blog/{id}')
+def show(id: int):
+    #fetch blog id = id
+    return {'data': id}
+
+
+@app.get('/about') #about page
 def about():
     return {
         'data': 'About FastAPI'
     }
+
+@app.get('/blog/{id}/comments')
+def comments(id, limit=10):
+    #fetch comments of blog with id = id
+    return {'data': {'1', '2'}}
+
+
+
+#@api -> operations decorator
+#get('/') -> Path
+#def about() -> Path operation functions
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool] = None
+
+
+@app.post('/blog', status_code=201)
+def create_blog(blog: Blog):
+    return { 'data': f'Blog was created with title as {blog.title}, and here is the content: {blog.body}' }
+
+
